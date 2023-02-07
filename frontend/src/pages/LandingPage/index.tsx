7c5c18ff0146f5c60f4c0ab566d6b439efebafdc
Mayk <maycon.lrgo@gmail.com>
2023-02-07 02:10:36 -0300

    feat(frontend): spinner loading

--- frontend/src/pages/LandingPage/index.tsx
+++ frontend/src/pages/LandingPage/index.tsx
@@ -1,9 +1,20 @@
+import { useState, useEffect } from "react";
 import Header from "../../components/organisms/Header";
 import Main from "./Main";
 import * as S from "./styles";
 
 function LandingPage() {
-  return (
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 500);
+  }, []);
+  return isLoading ? (
+    <S.SpinnerContainer>
+      <div className="spinner"></div>
+    </S.SpinnerContainer>
+  ) : (
     <S.Container>
       <Header />
       <Main />
