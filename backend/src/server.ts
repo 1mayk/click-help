96ebc71357c4604f2f56aaab5d31a2b40a630e29
Mayk <maycon.lrgo@gmail.com>
2023-02-10 20:27:30 -0300

    feat(backend): add debug e removendo comentarios

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,8 @@
 import 'dotenv/config';
 import app from './app';
-// import connectToDatabase from './models/connection';
 
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log("Role ta funcionando bem!");
+  console.log("Projeto Rodando!");
 });
-
-// connectToDatabase()
-  // .then(() => {
-  //   app.listen(PORT, () => console.log(`Running server on port: ${PORT}`));
-  // })
-  // .catch((error) => {
-  //   console.log('Connection with database generated an error:\r\n');
-  //   console.error(error);
-  //   console.log('\r\nServer initialization cancelled');
-  //   process.exit(0);
-  // });
